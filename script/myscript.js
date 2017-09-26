8777ff7acc0f3f6589e835ce3152fb747856182b
Nguyễn Đức Thuận <beard.thuan@1pac.vn>
2017-09-26 08:39:15 +0700

    udemy practice

--- script/myscript.js
+++ script/myscript.js
@@ -1,8 +1,32 @@
-document.onload = function () {
+document.addEventListener('DOMContentLoaded', function() {
   new Vue({
-    el:'#app',
+    el: '#exercise',
     data: {
-      message: "Hello VUE, HELLO WORLD!"
+      vfx: {
+        highlight: true,
+        shrink   : false
+      },
+      inputClass:'',
+      userClass: '',
+      isVisible: false,
+      inputClass2: '',
+      finish: false
+    },
+    methods: {
+      startEffect: function(){
+        var local = this;
+        setInterval(function() {
+          local.vfx.highlight = !local.vfx.highlight;
+          local.vfx.shrink = !local.vfx.shrink;
+        }, 1000);
+      },
+
+      startProgress: function() {
+        var local = this;
+        setInterval(function() {
+          local.finish = !local.finish;
+        }, 2000);
+      }
     }
   });
-};
+});

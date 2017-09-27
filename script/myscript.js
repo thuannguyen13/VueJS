60e43fee5cdcdf5df3ce7f0f75f11086a4840659
Nguyễn Đức Thuận <kenzilian13@gmail.com>
2017-09-27 16:34:39 +0700

    refactor code

--- script/myscript.js
+++ script/myscript.js
@@ -7,7 +7,8 @@ document.addEventListener('DOMContentLoaded', function(){
       spellCount : 2,
       castSpell : true,
       gameIsRunning: false,
-      missChance:''
+      missChance:'',
+      turns: []
   	},
     watch : {
       gameIsRunning : function(){
@@ -18,6 +19,10 @@ document.addEventListener('DOMContentLoaded', function(){
         if(this.spellCount === 0){
           this.castSpell = false;
         }
+      },
+
+      playerHealth : function(){
+        this.playerHealth > 100 ? this.playerHealth = 100 : '';
       }
     },
     computed: {
@@ -42,6 +47,9 @@ document.addEventListener('DOMContentLoaded', function(){
       resetGame : function(){
         this.gameIsRunning = false;
         this.playerHealth = this.monsterHealth = 100;
+        this.spellCount = 2;
+        this.castSpell = true;
+        this.turns = [];
       },
 
       // calculate damage
@@ -50,41 +58,64 @@ document.addEventListener('DOMContentLoaded', function(){
         return Math.round(Math.random() * 10);
       },
 
+      displayLog : function(isUser, actionType, unit){
+        if(isUser == true){
+          switch (actionType){
+            case "attack":
+              this.turns.unshift({
+                isPlayer: true,
+                text: 'Hero Attack Monster -' + unit
+              });
+              break;
+            case "spell":
+              this.turns.unshift({
+                isPlayer: true,
+                text: 'Hero Attack Monster with "Fire Ball" - ' + unit
+              });
+              break;
+            case "heal":
+              this.turns.unshift({
+                isPlayer: true,
+                text: 'Hero heal himself - ' + unit
+              });
+          }
+        } else {
+          this.turns.unshift({
+            isPlayer: false,
+            text: 'Monster attack Hero - ' + unit
+          });
+        }
+      },
+
+
+
       // player control
       monsterAttack : function(){
         var damage = this.calculateDamage();
+        this.displayLog(false, 'attack', damage);
         this.playerHealth -= damage;
-        if(damage === 0){
-          console.log('%c Monster Attack: MISS', 'color: red');
-        }else{
-          console.log('%c Monster Attack: ' + damage, 'color: red');
-        }
       },
 
   		playerAttack : function(){
         var damage = this.calculateDamage();
+        this.displayLog(true, 'attack', damage);
         this.monsterHealth -= damage;
-        if(damage === 0){
-          console.log('%c Player Attack: MISS ', 'color: lightgreen');
-        }else{
-          console.log('%c Player Attack: ' + damage, 'color: lightgreen');
-        }
-
   		},
 
       heal : function(){
         var health = this.calculateDamage();
         this.playerHealth += health;
         console.log('%c Player Health: ' + health, 'color: cyan');
+        this.displayLog(true, 'heal', health);
         this.monsterAttack();
       },
 
       spell : function(){
-        var spellDamage = this.calculateDamage() * 1.5;
-        this.monsterHealth -= spellDamage;
-        console.log('%c Play casted Spell: ' + spellDamage, 'color: orange');
+        var damage = this.calculateDamage() * 1.5;
+        this.monsterHealth -= damage;
         this.monsterAttack();
         this.spellCount--;
+        this.displayLog(true, 'spell', damage);
       }
 
   	}
